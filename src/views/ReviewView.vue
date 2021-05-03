812eb94d9da0f24b5af511c473a4bf6dde59d871
cole-adams <cole_adams@shaw.ca>
2021-05-03 17:56:43 -0600

    Add global css variable file and create page view files

---
+++ src/views/ReviewView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+  </div>
+</template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+})
+export default class ReviewView extends Vue {}
+</script>
+<style scoped>
+
+</style>
