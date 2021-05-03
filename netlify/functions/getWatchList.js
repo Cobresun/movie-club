29de7a766109ac101019feb75f96839dfd9fb6bb
Brian Norman <briankn8@gmail.com>
2021-05-03 16:30:39 -0600

    Use fauna for watch list

--- netlify/functions/getWatchList.js
+++ netlify/functions/getWatchList.js
@@ -1,30 +1,13 @@
-exports.handler = async function(event, context) {
-    const mockWatchList = [
-        {
-            movieTitle: "Die Hard", 
-            dateAdded: "Nov 1, 2020",
-            addedBy: "Brian",
-            dateReleased: "Jan 13, 1845",
-            genre: "Action/Adventure"
-        },
-        {
-            movieTitle: "2001: A Space Odyssey", 
-            dateAdded: "Nov 1, 2020",
-            addedBy: "Wes",
-            dateReleased: "Feb 13, 1623",
-            genre: "Thriller"
-        },
-        {
-            movieTitle: "Citizen Kane", 
-            dateAdded: "Nov 12, 2019",
-            addedBy: "Brian",
-            dateReleased: "Mar 3, 2200",
-            genre: "Thriller"
-        }
-    ]
+const faunadb = require('faunadb')
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(mockWatchList)
+const faunaClient = new faunadb.Client({ secret: process.env.FAUNADB_SERVER_SECRET })
+const q = faunadb.query
+
+exports.handler = async function(event, context) {
+    try {
+        const req = await faunaClient.query(q.Map(q.Paginate(q.Match(q.Index("all_watchList"))), q.Lambda("attr", q.Get(q.Var("attr")))))
+        return { statusCode: 200, body: JSON.stringify(req.data) }
+    } catch (err) {
+        return { statusCode: 500, body: JSON.stringify({ error: err.message}) }
     }
 }
