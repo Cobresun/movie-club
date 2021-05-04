add831a19a4c41bad0e8876ee845ba7799c7bf37
cole-adams <cole_adams@shaw.ca>
2021-05-03 19:03:20 -0600

    Add title to navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div id="app">
+    <nav-bar/>
     <router-view/>
   </div>
 </template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import NavBar from '@/components/NavBar.vue';
 
+@Options({
+  components: {
+    NavBar,
+  },
+})
+export default class App extends Vue {}
+</script>
 <style>
 @import './assets/styles/variables.css';
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -14,16 +26,7 @@
   color: var(--primary-color);
 }
 
-#nav {
+.nav {
   padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3 class="title">MovieClub</h3>
+  </div>
+</template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+})
+export default class NavBar extends Vue {}
+</script>
+<style scoped>
+.title {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 700;
+  font-size: 25px;
+  color: var(--primary-color);
+  text-align: left;
+  margin-left:20px;
+}
+</style>
