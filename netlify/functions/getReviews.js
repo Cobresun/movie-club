1122899ce1f68c3b846f81ad08bc712bddbf6a49
Brian Norman <briankn8@gmail.com>
2021-05-04 13:10:03 -0600

    Update endpoints to not send fauna metadata

--- netlify/functions/getReviews.js
+++ netlify/functions/getReviews.js
@@ -7,7 +7,13 @@ const q = faunadb.query
 exports.handler = async function(event, context) {
     try {
         const req = await faunaClient.query(q.Map(q.Paginate(q.Match(q.Index("all_reviews"))), q.Lambda("attr", q.Get(q.Var("attr")))))
-        return { statusCode: 200, body: JSON.stringify(req.data) }
+        let scrapedReviews = []
+
+        req.data.forEach(review => {
+            scrapedReviews.push(review.data)
+        });
+
+        return { statusCode: 200, body: JSON.stringify(scrapedReviews) }
     } catch (err) {
         return { statusCode: 500, body: JSON.stringify({ error: err.message}) }
     }
