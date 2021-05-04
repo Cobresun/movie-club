df17a728b0ac2ef72b8f44c058f6c1f0a7ec2838
wesley-tang <wesleytang1@gmail.com>
2021-05-04 16:16:44 -0600

    Fix link width

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
   <div class="home">
     <h1>Cobresun</h1>
     <btn @click="doStuff()">Sign In <mdicon name="arrow-right" /></btn>
-    <router-link to="/reviews"><h3>Reviews</h3></router-link>
-    <router-link to="/watchList"><h3>Watch List</h3></router-link>
+    <h3><router-link to="/reviews">Reviews</router-link></h3>
+    <h3><router-link to="/watchList">Watch List</router-link></h3>
   </div>
 </template>
 
