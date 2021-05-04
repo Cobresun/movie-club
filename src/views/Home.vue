60c29ebec8a133eee745538292e2a92c7d0b22ca
cole-adams <cole_adams@shaw.ca>
2021-05-04 13:04:42 -0600

    Add button example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>Cobresun</h1>
-
+    <btn @click="doStuff()">Sign In <mdicon name="arrow-right" /></btn>
     <router-link to="/reviews"><h3>Reviews</h3></router-link>
     <router-link to="/watchList"><h3>Watch List</h3></router-link>
   </div>
@@ -12,6 +12,9 @@ import { Options, Vue } from 'vue-class-component';
 
 @Options({
 })
-
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  doStuff(): void {
+    console.log("stuff");
+  }
+}
 </script>
