6fad7f12bff78f15895bf7a1f6fda663db573695
cole-adams <cole_adams@shaw.ca>
2022-05-14 16:08:56 -0600

    Change app to use setup script syntax

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,10 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, onMounted } from 'vue'
+<script setup lang="ts">
 import { useStore } from 'vuex';
 import NavBar from '@/components/NavBar.vue'
 
-export default defineComponent({
-  components: { NavBar },
-  setup() {
-    const store = useStore();
-    onMounted(() => {
-      store.dispatch('init');
-    });
-  },
-})
+const store = useStore();
+store.dispatch('init');
 </script>
