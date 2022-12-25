7d5a0971bea00f6ddb3d1ac08473bd41419f407c
Brian Norman <briankn8@gmail.com>
2022-12-25 10:08:39 -0700

    Rounded corners for modals

--- src/common/components/VModal.vue
+++ src/common/components/VModal.vue
@@ -3,7 +3,7 @@
     class="fixed z-50 top-0 left-0 w-full h-full bg-opacity-50 bg-black flex justify-center items-center"
     @click="emit('close')"
   >
-    <div class="bg-background p-8 h-2/3 w-1/2" @click.stop="">
+    <div class="rounded-lg bg-background p-8 h-2/3 w-1/2" @click.stop="">
       <slot />
     </div>
   </div>
