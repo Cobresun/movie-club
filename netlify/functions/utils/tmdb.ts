fc68ebd92a632baafae14d3083bc04293380a085
Brian Norman <briankn8@gmail.com>
2022-12-30 15:04:42 -0700

    Isolate TMDB_API_KEY to one function

--- netlify/functions/utils/tmdb.ts
+++ netlify/functions/utils/tmdb.ts
@@ -2,78 +2,56 @@ import { DetailedReviewResponse, ReviewResponse, WatchListItem } from "@/common/
 import axios from "axios";
 import { ReviewDatabaseObject } from "./types";
 
-const tmdbApiKey = process.env.TMDB_API_KEY
+async function makeTMDBApiCall(path: string): Promise<any> {
+    const tmdbApiKey = process.env.TMDB_API_KEY
+    return axios.get(`https://api.themoviedb.org/3${path}?api_key=${tmdbApiKey}`);
+}
 
 // TODO: don't export this, get rid of any external usages
 export async function getTMDBConfig() {
-    return axios
-        .get(`https://api.themoviedb.org/3/configuration?api_key=${tmdbApiKey}`)
+    return makeTMDBApiCall("/configuration");
 }
 
 export async function getTMDBMovieData(movieId: number) {
-    return axios
-        .get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${tmdbApiKey}`)
+    return makeTMDBApiCall(`/movie/${movieId}`);
 }
 
-export async function getReviewData(reviews: ReviewDatabaseObject[]) {
-    const promises: Promise<ReviewResponse>[] = [];
-    for (const movie of reviews) {
-        const promise = axios
-        .get(
-            `https://api.themoviedb.org/3/movie/${movie.movieId}?api_key=${tmdbApiKey}`
-        )
-        .then((response) => ({
-            ...movie,
-            movieTitle: response.data.title,
-        }));
-        promises.push(promise);
-    }
-
-    return await Promise.all(promises);
+export async function getReviewData(reviews: ReviewDatabaseObject[]): Promise<ReviewResponse[]> {
+    return Promise.all(
+        reviews.map(async (review) => {
+            const response = await makeTMDBApiCall(`/movie/${review.movieId}`);
+            return {
+                ...review,
+                movieTitle: response.data.title,
+            };
+        })
+    );
 }
 
-export async function getDetailedReviewData(reviews: DetailedReviewResponse[]) {
-    const promises = [];
+export async function getDetailedReviewData(reviews: DetailedReviewResponse[]): Promise<DetailedReviewResponse[]> {
+    const configuration = await getTMDBConfig();
 
-    const configuration = await getTMDBConfig()
-
-    for (const movie of reviews) {
-        const promise = axios
-        .get(
-            `https://api.themoviedb.org/3/movie/${movie.movieId}?api_key=${tmdbApiKey}`
-        )
-        .then((response) => {
-            movie.movieTitle = response.data.title;
-            movie.movieData = response.data;
-            movie.movieData.poster_url = `${configuration.data.images.secure_base_url}w154${response.data.poster_path}`;
-        });
-        promises.push(promise);
-    }
-
-    await Promise.all(promises);
+    await Promise.all(
+        reviews.map(async (review) => {
+            const response = await makeTMDBApiCall(`/movie/${review.movieId}`);
+            review.movieTitle = response.data.title;
+            review.movieData = response.data;
+            review.movieData.poster_url = `${configuration.data.images.secure_base_url}w154${response.data.poster_path}`;
+        })
+    );
     return reviews;
 }
 
-export async function getWatchlistItemMovieData(watchList: WatchListItem[]) {
-    const configuration = await getTMDBConfig()
+export async function getWatchlistItemMovieData(watchList: WatchListItem[]): Promise<WatchListItem[]> {
+    const configuration = await getTMDBConfig();
 
-    const promises = [];
-    for (const movie of watchList) {
-        const promise = axios
-        .get(
-            `https://api.themoviedb.org/3/movie/${movie.movieId}?api_key=${tmdbApiKey}`
-        )
-        .then((response) => {
+    await Promise.all(
+        watchList.map(async (movie) => {
+            const response = await makeTMDBApiCall(`/movie/${movie.movieId}`);
             movie.movieTitle = response.data.title;
             movie.releaseDate = response.data.release_date;
-            movie.poster_url =
-            configuration.data.images.base_url +
-            "w500" +
-            response.data.poster_path;
-        });
-        promises.push(promise);
-    }
-
-    await Promise.all(promises);
+            movie.poster_url = configuration.data.images.base_url + "w500" + response.data.poster_path;
+        })
+    );
     return watchList;
 }
