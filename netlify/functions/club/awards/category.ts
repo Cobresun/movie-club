24059594bdcd9683283816ca8effaee298f61f0f
Cole Adams <cole_adams@shaw.ca>
2023-03-27 17:34:42 -0600

    Add reorder and delete for category selection screen (#118)

--- netlify/functions/club/awards/category.ts
+++ netlify/functions/club/awards/category.ts
@@ -1,8 +1,10 @@
 import { updateClubAwardYear } from "./utils";
 import { secured } from "../../utils/auth";
-import { getFaunaClient } from "../../utils/fauna";
-import { badRequest, ok } from "../../utils/responses";
+import { getClubDocument, getFaunaClient } from "../../utils/fauna";
+import { badRequest, notFound, ok } from "../../utils/responses";
 import { Router } from "../../utils/router";
+
+import { BaseClubAwards } from "@/common/types/models";
 
 const router = new Router(
   "/api/club/:clubId<\\d+>/awards/:year<\\d+>/category"
@@ -21,6 +23,68 @@ router.post("/", secured, async (event, context, params) => {
       awards: q.Append(
         [{ title: body.title, nominations: [] }],
         q.Select("awards", q.Var("awardYear"))
+      ),
+    })
+  );
+
+  return ok();
+});
+
+router.put("/", secured, async (event, context, params) => {
+  if (!event.body) return badRequest("Missing body");
+  const body = JSON.parse(event.body);
+  if (!body.categories) return badRequest("Missing categories");
+
+  const { categories } = body as { categories: string[] };
+
+  const clubId = parseInt(params.clubId);
+  const year = parseInt(params.year);
+  const { faunaClient, q } = getFaunaClient();
+
+  const clubAwards = await faunaClient.query<BaseClubAwards | null>(
+    q.Select(
+      0,
+      q.Filter(
+        q.Select(["data", "clubAwards"], getClubDocument(clubId)),
+        q.Lambda("x", q.Equals(q.Select(["year"], q.Var("x")), year))
+      ),
+      null
+    )
+  );
+
+  if (clubAwards) {
+    const updatedAwards = categories.map((category) =>
+      clubAwards.awards.find((award) => award.title === category)
+    );
+    if (updatedAwards.some((award) => !award)) {
+      return badRequest(
+        "One or more of the category titles you provided does not exist"
+      );
+    }
+
+    await faunaClient.query(
+      updateClubAwardYear(clubId, year, { awards: updatedAwards })
+    );
+    return ok();
+  } else {
+    return notFound();
+  }
+});
+
+router.delete("/:awardTitle", secured, async (event, context, params) => {
+  const clubId = parseInt(params.clubId);
+  const year = parseInt(params.year);
+  const awardTitle = params.awardTitle;
+  const { faunaClient, q } = getFaunaClient();
+
+  await faunaClient.query(
+    updateClubAwardYear(clubId, year, {
+      awards: q.Filter(
+        q.Select("awards", q.Var("awardYear")),
+        q.Lambda(
+          "award",
+          q.Not(q.Equals(q.Select("title", q.Var("award")), awardTitle))
+        )
       ),
     })
   );
