e45c19ad3d575d449d37b65049a728544561030b
Brian Norman <briankn8@gmail.com>
2023-04-03 08:44:26 -0400

    Comments

--- src/common/searchMovies.ts
+++ src/common/searchMovies.ts
@@ -18,53 +18,51 @@ import { DetailedMovie } from "@/common/types/models";
  * 
  */
 export function filterMovies(reviews: DetailedMovie[], searchQuery: string): DetailedMovie[] {
-    if (!searchQuery) {
-      return reviews;
-    }
+  let filteredReviews = reviews
 
-    // If the search query has text followed by a colon, extract that out into a map
-    // of filters. Otherwise, just return an empty map. Type the map as a Record
-    // so that TypeScript knows the keys are strings and the values are strings.
-    const filters = searchQuery.includes(':')
-      ? searchQuery.split(' ').reduce((acc, filter) => {
-          const [key, value] = filter.split(':');
-          acc[key] = value;
-          return acc;
-        }, {} as Record<string, string>)
-      : {};
+  // If the search query has text followed by a colon, extract that out into a map
+  // of filters. Otherwise, just return an empty map. Type the map as a Record
+  // so that TypeScript knows the keys are strings and the values are strings.
+  const filters = searchQuery.includes(':')
+    ? searchQuery.split(' ').reduce((acc, filter) => {
+        const [key, value] = filter.split(':');
+        acc[key] = value;
+        return acc;
+      }, {} as Record<string, string>)
+    : {};
 
-    // If there are filters, remove the filter and the value from the search query.
-    if (Object.keys(filters).length) {
-      searchQuery = searchQuery.replace(/(\w+:\w+\s?)/g, '');
-    }
-
-    // If there are filters, filter the reviews by them.
-    if (filters.title) {
-      reviews = reviews.filter(review => review.movieTitle.toLowerCase().includes(filters.title.toLowerCase()));
-    }
-    if (filters.company) {
-      reviews = reviews.filter(review =>
-        review.movieData.production_companies.some(company => company.name.toLocaleLowerCase().includes(filters.company.toLowerCase()))
-      );
-    }
-
-    if (filters.genre) {
-      reviews = reviews.filter(review =>
-        review.movieData.genres.some(genre => genre.name.toLocaleLowerCase().includes(filters.genre.toLowerCase()))
-      );
-    }
-
-    // Now any text after the filters is a search query. If there is a search query,
-    // filter the reviews by it.
-    if (searchQuery) {
-      reviews = reviews.filter(review => {
-        const { movieTitle } = review;
-
-        return (
-          movieTitle.toLowerCase().includes(searchQuery.toLowerCase())
-        );
-      });
-    }
-
-    return reviews
+  // If there are filters, remove the filter and the value from the search query.
+  if (Object.keys(filters).length) {
+    searchQuery = searchQuery.replace(/(\w+:\w+\s?)/g, '');
   }
+
+  // If there are filters, filter the reviews by them.
+  if (filters.title) {
+    filteredReviews = filteredReviews.filter(review => review.movieTitle.toLowerCase().includes(filters.title.toLowerCase()));
+  }
+  if (filters.company) {
+    filteredReviews = filteredReviews.filter(review =>
+      review.movieData.production_companies.some(company => company.name.toLocaleLowerCase().includes(filters.company.toLowerCase()))
+    );
+  }
+
+  if (filters.genre) {
+    filteredReviews = filteredReviews.filter(review =>
+      review.movieData.genres.some(genre => genre.name.toLocaleLowerCase().includes(filters.genre.toLowerCase()))
+    );
+  }
+
+  // Now any text after the filters is a search query. If there is a search query,
+  // filter the reviews by it.
+  if (searchQuery) {
+    filteredReviews = filteredReviews.filter(review => {
+      const { movieTitle } = review;
+
+      return (
+        movieTitle.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    });
+  }
+
+  return filteredReviews
+}
