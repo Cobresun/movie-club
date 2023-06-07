aa3939e76ce55383461e589f6362a998896ef53d
Cole Adams <cole_adams@shaw.ca>
2023-06-07 14:27:15 -0600

    Add tanstack query ESlint plugin and fix any issues (#135)

--- src/service/useReview.ts
+++ src/service/useReview.ts
@@ -25,57 +25,53 @@ export function useSubmitScore(clubId: string) {
   const { data: user } = useUser();
   const auth = useAuthStore();
   const queryClient = useQueryClient();
-  return useMutation(
-    ({ movieId, score }: { movieId: number; score: number }) =>
+  return useMutation({
+    mutationFn: ({ movieId, score }: { movieId: number; score: number }) =>
       auth.request.put(`/api/club/${clubId}/reviews/${movieId}`, {
         name: user.value?.name,
         score: score,
       }),
-    {
-      onMutate: async ({ movieId, score }) => {
-        await queryClient.cancelQueries(["review-d", clubId]);
-        queryClient.setQueryData<DetailedReviewResponse[]>(
-          ["review-d", clubId],
-          (currentReviews) => {
-            const name = user.value?.name;
-            if (!currentReviews || !name) return currentReviews;
-            return currentReviews.map((review) =>
-              review.movieId === movieId
-                ? {
-                    ...review,
-                    scores: { ...review.scores, [name]: score },
-                  }
-                : { ...review }
-            );
-          }
-        );
-      },
-      onSettled: () => {
-        queryClient.invalidateQueries({ queryKey: ["review-d", clubId] });
-      },
-    }
-  );
+    onMutate: async ({ movieId, score }) => {
+      await queryClient.cancelQueries(["review-d", clubId]);
+      queryClient.setQueryData<DetailedReviewResponse[]>(
+        ["review-d", clubId],
+        (currentReviews) => {
+          const name = user.value?.name;
+          if (!currentReviews || !name) return currentReviews;
+          return currentReviews.map((review) =>
+            review.movieId === movieId
+              ? {
+                  ...review,
+                  scores: { ...review.scores, [name]: score },
+                }
+              : { ...review }
+          );
+        }
+      );
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["review-d", clubId] });
+    },
+  });
 }
 
 export function useAddReview(clubId: string) {
   const auth = useAuthStore();
   const queryClient = useQueryClient();
-  return useMutation(
-    (movieId: number) =>
+  return useMutation({
+    mutationFn: (movieId: number) =>
       auth.request.post(`/api/club/${clubId}/reviews/${movieId}`),
-    {
-      onSuccess: (response) => {
-        queryClient.setQueryData<DetailedReviewResponse[]>(
-          ["review-d", clubId],
-          (currentReviews) => {
-            if (!currentReviews) return [response.data];
-            return [response.data, ...currentReviews];
-          }
-        );
-      },
-      onSettled: () => {
-        queryClient.invalidateQueries({ queryKey: ["review-d", clubId] });
-      },
-    }
-  );
+    onSuccess: (response) => {
+      queryClient.setQueryData<DetailedReviewResponse[]>(
+        ["review-d", clubId],
+        (currentReviews) => {
+          if (!currentReviews) return [response.data];
+          return [response.data, ...currentReviews];
+        }
+      );
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["review-d", clubId] });
+    },
+  });
 }
