9dda7dcb1912bb336f0773c1053337012f711575
cole-adams <colepadams99@gmail.com>
2024-03-02 09:15:25 -0700

    WIP: Lists migration script

---
+++ migrations/schema/20240227_AddLists.ts
@@ -0,0 +1,60 @@
+import { Kysely } from "kysely";
+
+export async function up(db: Kysely<unknown>) {
+  // Work
+  await db.schema
+    .createTable("work")
+    .addColumn("id", "serial", (col) => col.primaryKey())
+    .addColumn("club_id", "int8", (col) => col.notNull())
+    .addForeignKeyConstraint(
+      "fk_work_club_id",
+      ["club_id"],
+      "club",
+      ["id"],
+      (cb) => cb.onDelete("cascade")
+    )
+    .addColumn("type", "varchar(50)", (col) => col.notNull())
+    .addColumn("title", "varchar(255)", (col) => col.notNull())
+    .addColumn("external_id", "varchar(255)")
+    .addColumn("image_url", "varchar(255)")
+    .execute();
+
+  // List
+  await db.schema
+    .createTable("work_list")
+    .addColumn("id", "serial", (col) => col.primaryKey())
+    .addColumn("club_id", "int8", (col) => col.notNull())
+    .addForeignKeyConstraint(
+      "fk_work_list_club_id",
+      ["club_id"],
+      "club",
+      ["id"],
+      (cb) => cb.onDelete("cascade")
+    )
+    .addColumn("type", "varchar(50)", (col) => col.notNull())
+    .addColumn("title", "varchar(255)")
+    .execute();
+
+  // List items
+  await db.schema
+    .createTable("work_list_item")
+    .addColumn("list_id", "int8")
+    .addColumn("work_id", "int8")
+    .addColumn("created_date", "datetime", (col) => col.notNull())
+    .addPrimaryKeyConstraint("pk_work_list_item", ["list_id", "work_id"])
+    .addForeignKeyConstraint(
+      "fk_work_list_item_list_id",
+      ["list_id"],
+      "list",
+      ["id"],
+      (cb) => cb.onDelete("cascade")
+    )
+    .addForeignKeyConstraint(
+      "fk_work_list_item_work_id",
+      ["work_id"],
+      "work",
+      ["id"],
+      (cb) => cb.onDelete("cascade")
+    )
+    .execute();
+}
