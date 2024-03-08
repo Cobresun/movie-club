9adf7eb57ac4a41230663b4347ce1123bc40d861
cole-adams <colepadams99@gmail.com>
2024-03-08 00:14:14 -0700

    Add backlog data migration script

--- migrations/data/20240303_MigrateBacklog.ts
+++ migrations/data/20240303_MigrateBacklog.ts
@@ -16,6 +16,7 @@ const migrateBacklog = async () => {
   );
 
   for (const club of clubs.data) {
+    console.log(`Migrating backlog for club ${club.data.clubName}`);
     // Find corresponding club in CockroachDB by legacy_id
     const [cockroachClub] = await db
       .selectFrom("club")
@@ -39,6 +40,18 @@ const migrateBacklog = async () => {
 
       for (const movie of detailedMovies) {
         // Insert work if not already present
+        const existingWork = await db
+          .selectFrom("work")
+          .select("id")
+          .where("external_id", "=", movie.movieId.toString())
+          .where("club_id", "=", cockroachClub.id)
+          .executeTakeFirst();
+
+        if (existingWork) {
+          continue;
+        }
+
+        console.log(`Inserting work for movie ${movie.movieTitle}`);
         const [workId] = await db
           .insertInto("work")
           .values({
@@ -57,7 +70,7 @@ const migrateBacklog = async () => {
           .values({
             list_id: workListId.id,
             work_id: workId.id,
-            created_date: movie.timeAdded["@ts"], // Convert FaunaDB timestamp to JavaScript Date
+            created_date: movie.timeAdded?.date ?? movie.timeWatched?.date, // Convert FaunaDB timestamp to JavaScript Date
           })
           .execute();
       }
