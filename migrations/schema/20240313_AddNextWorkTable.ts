204f7f404f7a49c6722f0ff73bbaa611ced93966
cole-adams <colepadams99@gmail.com>
2024-03-13 23:26:45 -0600

    Add migrations for watchlist and next work tables

---
+++ migrations/schema/20240313_AddNextWorkTable.ts
@@ -0,0 +1,37 @@
+import { Kysely } from "kysely";
+
+export async function up(db: Kysely<unknown>) {
+  await db.schema
+    .createTable("next_work")
+    .addColumn("work_id", "int8", (col) => col.notNull())
+    .addColumn("club_id", "int8", (col) => col.notNull())
+    .addPrimaryKeyConstraint("pk_next_work", ["work_id", "club_id"])
+    .addForeignKeyConstraint(
+      "fk_next_work_club_id",
+      ["club_id"],
+      "club",
+      ["id"],
+      (cb) => cb.onDelete("cascade")
+    )
+    .addForeignKeyConstraint(
+      "fk_next_work_work_id",
+      ["work_id"],
+      "work",
+      ["id"],
+      (cb) => cb.onDelete("cascade")
+    )
+    .execute();
+
+  await db.schema
+    .alterTable("work")
+    .addUniqueConstraint("uq_club_id_type_external_id", [
+      "club_id",
+      "type",
+      "external_id",
+    ])
+    .execute();
+}
+
+export async function down(db: Kysely<unknown>) {
+  await db.schema.dropTable("next_work").execute();
+}
