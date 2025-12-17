328c6f02cb8ad4904a45a31df40678756d7c6eb6
Brian Norman <briankn8@gmail.com>
2025-12-17 01:11:54 -0500

    Make navigation cards responsive with CSS Grid layout (#212)

--- src/features/clubs/components/MenuCard.vue
+++ src/features/clubs/components/MenuCard.vue
@@ -1,10 +1,10 @@
 <template>
   <button
-    class="rounded-xl p-3 text-2xl tracking-wide duration-150 hover:brightness-105 active:brightness-110"
+    class="w-full rounded-xl p-2 text-base tracking-wide duration-150 hover:brightness-105 active:brightness-110 md:p-3 md:text-xl"
     :class="`bg-${bgColor}`"
     @click="emit('click')"
   >
-    <img :src="image" class="mx-auto mb-2 h-24 w-48" />
+    <img :src="image" class="mx-auto mb-1 h-16 w-32 md:mb-2 md:h-20 md:w-40" />
     <div class="prompt">
       <slot />
     </div>
