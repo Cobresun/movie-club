7b0d249050d600eb2886e55f51af7e46a1b36630
Cole Adams <cadams@xmentium.com>
2026-02-01 17:00:49 -0700

    log: Log path info to see what's going

--- netlify/functions/utils/database.ts
+++ netlify/functions/utils/database.ts
@@ -19,6 +19,10 @@ export function getDbUrl(): string | undefined {
   try {
     // In Lambda/Netlify Functions, check for database-config.json next to this file
     const configPath = path.resolve(__dirname, "./database-config.json");
+    console.log("__dirname:", __dirname);
+    console.log("process.cwd():", process.cwd());
+    console.log("Trying config path:", configPath);
+    console.log("File exists?:", existsSync(configPath));
     if (existsSync(configPath)) {
       const configText = readFileSync(configPath, "utf8");
       const config = configFileSchema.parse(JSON.parse(configText));
